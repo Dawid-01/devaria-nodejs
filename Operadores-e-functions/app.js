b9fa7d1e11d5244c0d948f70dacbf4f0dc127cef
Dawid de Jesus Oliveira <dawidjesus2@gmail.com>
2023-04-20 17:09:19 -0300

    Create app.js
    Subindo exercicio de operadores e functions, falta terminar.

---
+++ Operadores-e-functions/app.js
@@ -0,0 +1,28 @@
+const readLine = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const validarNumeroInformado = (numero) => {
+try{
+            return Number.parseFloat(numero);
+        } catch(e){
+            console.log('numero informado não é valido');
+        }
+}
+
+
+readLine.question('Favor informar um número:', (numero1) => {
+   
+const numeroValidado1 = validarNumeroInformado(numero1);
+ 
+if(numeroValidado1)
+    readLine.question('Favor informar outro numero:' (numero2) => {
+            const numeroValidado2 = validarNumeroInformado(numero2);    
+
+
+            
+   });
+   
+
+});
